985c5afab0d6a090e16f02821962eeacf2f20d51
arkangelofkaos <arkangelofkaos@gmail.com>
2013-07-01 19:31:13 +0100

    refactor to remove duplication and use set up

--- test/ListUtilTest.java
+++ test/ListUtilTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -10,21 +11,24 @@ import static org.junit.Assert.assertEquals;
  */
 public class ListUtilTest {
 
+    private List<Integer> orderedListTestData;
+
+    @Before
+    public void setUp() throws Exception {
+        orderedListTestData = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+    }
+
     @Test
     public void shouldReturnIndexOfItem7() throws Exception {
-        List<Integer> orderedList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
-        assertEquals(6, indexOf(7, orderedList));
+        assertEquals(6, indexOf(7, orderedListTestData));
     }
 
     @Test
     public void searcItemNotFoundReturnMinusOne() throws Exception {
-        List<Integer> orderedList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
-        assertEquals(-1, indexOf(9, orderedList));
-
+        assertEquals(-1, indexOf(9, orderedListTestData));
     }
 
     private <T> int indexOf(T searchItem, List<T> orderedList) {
-
         if (orderedList.indexOf(searchItem) >= 0) {
             return 6;
         } else {
