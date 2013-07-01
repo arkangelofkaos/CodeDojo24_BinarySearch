9588bd6b426db473769f0dd847333c06a9c8678b
arkangelofkaos <arkangelofkaos@gmail.com>
2013-07-01 23:12:08 +0100

    starting binary search impl, ensuring we have correct aim with comments planning outcome/route

--- test/ListUtilTest.java
+++ test/ListUtilTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -10,9 +11,15 @@ import static org.junit.Assert.assertEquals;
  */
 public class ListUtilTest {
 
+    private List<Integer> sortedListTestData;
+
+    @Before
+    public void setUp() throws Exception {
+        sortedListTestData = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+    }
+
     @Test
     public void shouldReturnIndexOfFoundItem() throws Exception {
-        List<Integer> sortedListTestData = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
         int expectedIndex = 6;
         int searchItem = 7;
         int actualIndexOfItem = indexOf(searchItem, sortedListTestData);
@@ -22,7 +29,6 @@ public class ListUtilTest {
 
     @Test
     public void shouldReturnMinusOneForMissingItem() throws Exception {
-        List<Integer> sortedListTestData = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
         int expectedIndex = -1;
         int searchItem = 10;
         int actualIndexOfItem = indexOf(searchItem, sortedListTestData);
@@ -30,7 +36,28 @@ public class ListUtilTest {
         assertEquals(expectedIndex, actualIndexOfItem);
     }
 
+    // Binary search:
+    // If empty list return -1;
+    // If start == end, return -1 if list item isn't the search item
+    // If start == end, return the start if list item isn't the search item
+    // If start != end, get the middle point
+    // --> if item@mid point >= search item, recurse with start & midpoint
+    // --> if item@mid point < search item, recurse with midpoint + 1 & end
+
+
+    @Test
+    public void shouldCalculateMiddleIndexOf3GivenListOfSize7() throws Exception {
+        int middleIndex = middleIndexOf(Arrays.asList(1, 2, 3, 4, 5, 6, 7));
+        assertEquals(3, middleIndex);
+    }
+
     private <T> int indexOf(T item, List<T> sortedList) {
         return sortedList.indexOf(item);
     }
+
+    private int middleIndexOf(List<?> list) {
+        return 3;
+    }
+
+
 }
